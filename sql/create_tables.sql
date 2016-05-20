6f651b7fdd94f415f2493d30f553a94483275478
Kyle Colantonio <kyle10468@gmail.com>
2016-05-20 00:48:23 -0400

    Updated SQL Scripts

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 05, 2016 at 10:14 AM
--- Server version: 5.5.47-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
+-- Generation Time: May 02, 2016 at 01:22 PM
+-- Server version: 5.5.49-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,8 +32,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `hash` varchar(255) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `permission_level` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `activated` tinyint(1) NOT NULL DEFAULT '0',
+  `permission_level` tinyint(3) NOT NULL DEFAULT '0',
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`,`hash`)
@@ -49,12 +48,13 @@ CREATE TABLE IF NOT EXISTS `account_tokens` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(7) unsigned NOT NULL,
   `token` char(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `type` enum('LOGIN','REGISTER','RECOVERY') COLLATE utf8_unicode_ci NOT NULL,
+  `type` enum('LOGIN','REGISTER','RECOVERY','API') COLLATE utf8_unicode_ci NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_on` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `answers` (
   `answer` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `question_id` (`question_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `questions` (
   `question` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `set_id` (`set_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -97,7 +97,24 @@ CREATE TABLE IF NOT EXISTS `question_sets` (
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `responses`
+--
+
+CREATE TABLE IF NOT EXISTS `responses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(7) NOT NULL,
+  `sess_id` int(11) unsigned NOT NULL,
+  `ans_id` int(11) unsigned NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `sess_id` (`sess_id`,`ans_id`),
+  KEY `FK_R_ANSID_ID` (`ans_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -107,13 +124,13 @@ CREATE TABLE IF NOT EXISTS `question_sets` (
 
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `question_set` int(11) unsigned NOT NULL,
+  `set_id` int(11) unsigned NOT NULL,
   `token` char(8) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `current_question` int(11) unsigned NOT NULL,
+  `current_question` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`),
-  KEY `question_set` (`question_set`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `set_id` (`set_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
@@ -144,10 +161,17 @@ ALTER TABLE `question_sets`
   ADD CONSTRAINT `FK_QS_USERID-ID` FOREIGN KEY (`user_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `responses`
+--
+ALTER TABLE `responses`
+  ADD CONSTRAINT `FK_R_ANSID_ID` FOREIGN KEY (`ans_id`) REFERENCES `answers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_R_SESSID_ID` FOREIGN KEY (`sess_id`) REFERENCES `sessions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `sessions`
 --
 ALTER TABLE `sessions`
-  ADD CONSTRAINT `FK_S_QS_ID` FOREIGN KEY (`question_set`) REFERENCES `question_sets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_S_QS_ID` FOREIGN KEY (`set_id`) REFERENCES `question_sets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
